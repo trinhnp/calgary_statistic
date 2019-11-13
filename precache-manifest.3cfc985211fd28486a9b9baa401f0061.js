3c8b1c22ca4aff7da155592efe4ab8492bb1b8d8
Trinhnp <trinhnp3@gmail.com>
2019-11-13 14:45:09 -0700

    Updates

---
+++ precache-manifest.3cfc985211fd28486a9b9baa401f0061.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b205d0779ab98397543a306dd110e0",
+    "url": "/calgary_statistics/index.html"
+  },
+  {
+    "revision": "0f0de68c6338161efc19",
+    "url": "/calgary_statistics/static/css/main.974f916f.chunk.css"
+  },
+  {
+    "revision": "5060d1d76d58a7611e8d",
+    "url": "/calgary_statistics/static/js/2.d3fe4f7c.chunk.js"
+  },
+  {
+    "revision": "0f0de68c6338161efc19",
+    "url": "/calgary_statistics/static/js/main.2ead786c.chunk.js"
+  },
+  {
+    "revision": "291aeb3b27c5bed2d207",
+    "url": "/calgary_statistics/static/js/runtime-main.6aa941e3.js"
+  }
+]);
